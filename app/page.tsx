5312cf45d13fd6c7e09ce787f2c602ee45ff73d5
benalleng <benalleng@gmail.com>
2024-09-23 12:28:35 -0400

    feat: add base copy

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 'use client'
 
 import { useState } from 'react'
-import { LessonInfo, OpCodeParser } from 'ui'
+import Link from 'next/link'
+import { LessonInfo, OpCodeParser, HeroTitle, Icon } from 'ui'
 import { SuccessNumbers } from 'types'
 
 export default function Home() {
@@ -15,7 +16,75 @@ export default function Home() {
         setSuccess={setSuccess} 
       >
         <LessonInfo>
-          Hello world
+          <div className="flex flex-col md:pt-5 gap-5 font-nunito">
+            <div className="flex flex-row gap-[15px]">
+              <HeroTitle />
+              <Icon icon="cross" className="flex items-center"/>
+              <h1 className="text-[24px] font-bold">
+                Bitcoin script
+              </h1>
+            </div>
+            <div className="text-[22px] font-medium opacity-80">
+              <p>
+                Welcome to the bitcoin script editor & visualizer. This is your playground for learning and experimentation.
+              </p>
+              <p className="mt-6">
+                Try out some examples:
+              </p>
+              <ul className="list-disc list-inside ml-2.5">
+                <li>
+                  Basic transaction
+                </li>
+                <li>
+                  Multi-sig transaction
+                </li>
+                <li>
+                  Timelocked transaction
+                </li>
+              </ul>
+              <p className="mt-6">
+                Helpful links:
+              </p>
+              <ul className="list-disc list-inside ml-2.5">
+                <li>
+                  <Link
+                    href="https://learnmeabitcoin.com/technical/script"
+                    target="_blank"
+                    className="underline"
+                  >
+                    Introduction to bitcoin script
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://opcodeexplained.com/opcodes"
+                    target="_blank"
+                    className="underline"
+                  >
+                    OP_CODE documentation
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://savingsatoshi.com"
+                    target="_blank"
+                    className="underline"
+                  >
+                    Learn bitcoin script in Saving Satoshi
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://github.com/saving-satoshi/saving-satoshi-script"
+                    target="_blank"
+                    className="underline"
+                  >
+                    View the source code & contribute
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
         </LessonInfo>
       </OpCodeParser>
     </div>
--- ui/shared/icons/index.ts
+++ ui/shared/icons/index.ts
@@ -1,1 +1,3 @@
 export { default as refresh } from './Refresh'
+export { default as cross } from './Cross'
+export { default as play } from './Play'
