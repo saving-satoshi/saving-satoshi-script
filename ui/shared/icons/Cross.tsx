04857ff9f6c152cfa97c495ee18c7a3793964ad7
benalleng <benalleng@gmail.com>
2024-09-23 12:34:14 -0400

    chore: upgrade next version and catch some errant prop elements

--- ui/shared/icons/Cross.tsx
+++ ui/shared/icons/Cross.tsx
@@ -1,8 +1,13 @@
 export default function CrossIcon({ className }: { className: string }) {
   return (
-    <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M13 1L1 13" stroke="white" stroke-width="1.5"/>
-      <path d="M1 1L13 13" stroke="white" stroke-width="1.5"/>
+    <svg 
+      width="14" 
+      height="14" 
+      viewBox="0 0 14 14" 
+      fill="none" 
+      xmlns="http://www.w3.org/2000/svg">
+      <path d="M13 1L1 13" stroke="white" strokeWidth="1.5"/>
+      <path d="M1 1L13 13" stroke="white" strokeWidth="1.5"/>
     </svg>
   )
 }
