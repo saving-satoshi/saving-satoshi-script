461568def330af6203a331a6d2917a83c394f1f0
benalleng <benalleng@gmail.com>
2024-11-14 14:47:42 -0500

    fix fallback styling

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,9 @@ export default function Home() {
         fallback={
           <div className="flex h-full flex-col md:flex-row">
             <div className="max-w-full grow justify-center overflow-y-auto text-white md:max-w-[50%] md:basis-1/3">
-              <LessonFallback />
+              <div className="flex flex-col content-center gap-1 overflow-y-auto px-1 py-6 sm:px-12">
+                <LessonFallback />
+              </div>
             </div>
             <div className="max-w-full grow justify-center overflow-y-auto text-white md:max-w-[50%] md:basis-1/3">
               <div className="flex hidden h-[calc(100vh-48px)] items-center justify-center border-white/25 md:flex md:h-[calc(100vh)] md:max-w-[50vw] md:border-l">
